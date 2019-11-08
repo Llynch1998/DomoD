c821daec91351baa8ba6f090f0a6edaa6f032bb7
Llynch1998 <llynch1998@gmail.com>
2019-11-08 16:49:49 -0500

    added circle  ci

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -16,7 +16,7 @@ const makerPage = (req, res) => {
 };
 
 const makeDomo = (req,res) =>{
-    if(!req.body.name || !req.body.age){
+    if(!req.body.name || !req.body.age || !req.body.powerLevel){
         return res.status(400).json({error: 'RAWR! Both name and age are required'});
     }
     const domoData = {
